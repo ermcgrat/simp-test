356d13b676303889d29872a8f8d56a546e47fc31
Eric McGrath <emcgrath.shiftdigital@outlook.com>
2018-06-15 12:25:39 -0400

    Import and basic use of sd-grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { IGridColumnDef } from 'sd-grid';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'app';
+
+  cols: IGridColumnDef[] = [
+    {
+      name: 'First Name',
+      prop: 'firstName',
+      width: 100
+    },
+    {
+      name: 'Last Name',
+      prop: 'lastName',
+      width: 100
+    }
+  ];
+
+  rows = [
+    {
+      firstName: 'John',
+      lastName: 'Smith'
+    },
+    {
+      firstName: 'Jane',
+      lastName: 'Doe'
+    }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SdGridModule } from 'sd-grid';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SdGridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
